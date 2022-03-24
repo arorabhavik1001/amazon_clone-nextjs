4597b29fcefd2b2f2f889aaea69596b27bfddc3b
Bhavik Arora <arora.bhavik1001@gmail.com>
2022-03-24 17:23:44 +0530

    update 2.0

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,15 +7,19 @@ function Header() {
     alert("search not available");
   };
   return (
-    <header>
+    <header style={{ position: "sticky", top: 0 }} className="z-50">
       <div
         style={{
           "border-radius": "0px 0px 30px 30px",
           "background-color": "#233448",
           "border-bottom": "7px solid #FE9B00",
           "box-shadow": "3px 3px 13px -2px ",
+          "justify-content": "space-between",
+          "padding-top": "1rem",
+          "padding-bottom": "1.5rem",
+          "flex-grow": 1,
         }}
-        className="sticky top-0 z-50 sm:flex p-2 sm:p-5 flex-grow justify-between py-5 pt-4"
+        className="sm:flex p-2 sm:p-5 "
       >
         <div className="mt-2 flex items-center flex-grow sm:flex-grow-0 justify-between md:ml-10">
           <Image
@@ -31,12 +35,13 @@ function Header() {
             </div>
             <div className="flex items-center cursor-pointer hover:underline bg-yellow-400 rounded-full text-black px-3 space-x-3 py-1">
               <div className="relative">
-              <span className="absolute top-0 right-0 h-4 w-4 bg-yellow-600 rounded-full text-center text-md font-bold">2</span>
+                <span className="absolute top-0 right-0 h-4 w-4 bg-yellow-600 rounded-full text-center text-md font-bold">
+                  2
+                </span>
                 <ShoppingCartIcon className="h-8 badaHoja" />
               </div>
               <MenuIcon className="h-8 badaHoja" />
             </div>
-            
           </div>
         </div>
         <div className="bg-yellow-400 hover:bg-yellow-500 flex flex-grow cursor-pointer items-center justify-around max-w-4xl rounded-full mx-2 sm:mx-5 mt-2 p-0">
@@ -56,7 +61,9 @@ function Header() {
           </div>
           <div className="flex items-center cursor-pointer hover:underline bg-yellow-400 rounded-full text-black px-3 space-x-3 py-1">
             <div className="relative">
-              <span className="absolute top-0 right-0 h-4 w-4 bg-yellow-600 rounded-full text-center text-md font-bold">2</span>
+              <span className="absolute top-0 right-0 h-4 w-4 bg-yellow-600 rounded-full text-center text-md font-bold">
+                2
+              </span>
               <ShoppingCartIcon className="h-10 badaHoja" />
             </div>
             <MenuIcon className="h-9 badaHoja" />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,38 @@
 import Head from "next/head";
-import Header from "./Header";
+import Header from "../Components/Header";
+import Banner from "../Components/Banner";
+import ProductFeed from "../Components/ProductFeed";
 
 export default function Home() {
+  const search = () => {
+    alert("search not available");
+  };
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0 by Bhavik</title>
-        <link rel="shortcut icon" href="https://admin.itsnicethat.com/images/4YEKLST2YLtZqyvTssrmZ897tZE=/199226/width-1440%7Cformat-jpeg/amazon_old_app_icon_graphic_design_itsnicethat.png"></link>
+        <link
+          rel="shortcut icon"
+          href="https://admin.itsnicethat.com/images/4YEKLST2YLtZqyvTssrmZ897tZE=/199226/width-1440%7Cformat-jpeg/amazon_old_app_icon_graphic_design_itsnicethat.png"
+        ></link>
       </Head>
       <Header />
-
+      
+      <main className="max-w-screen-2xl mx-auto">
+        <Banner />
+        <ProductFeed />
+      </main>
     </div>
   );
 }
+
+// export async function getServerSideProps(context) {
+//   const products = await fetch('https://fakestoreapi.com/products').then(
+//     (res) => res.json()
+//   )
+//   return {
+//     props: {
+//       products,
+//     }
+//   }
+// }
